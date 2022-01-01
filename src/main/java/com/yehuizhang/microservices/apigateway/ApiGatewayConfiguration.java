cfe1ddbc81d6dd577b9c4ae4794325775f617065
Yehui Zhang <yehuizhang@outlook.com>
2022-01-01 14:07:17 -0800

    improved APiGateway routing urls

---
+++ src/main/java/com/yehuizhang/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,24 @@
+package com.yehuizhang.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyHeaderVal")
+                                .addRequestParameter("MyParam", "myParam Value")
+                        ).uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
